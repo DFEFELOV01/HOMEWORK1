6c06005b6a79efa80609e280a6682f183383f610
DFEFELOV01 <dima.fefelov97.fefelov@mail.ru>
2024-06-16 18:07:57 +0500

    Update main.py

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,40 +1,36 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
+# Домашнее задание №1
+# Функции и структуры данных
+
 # filter types
-ODD = "odd"
+ODD = "ODD"
 EVEN = "even"
 PRIME = "prime"
 
-
 def power_numbers(*args):
+    # Возвращает список квадратов переданных чисел
     return [x**2 for x in args]
 
+def is_prime(x):
+    # Функция, которая проверяет, является ли число x простым
+    if x <= 1:
+        return False
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
 def filter_numbers(numbers, filter_type):
+    # Фильтрует список чисел в зависимости от типа фильтрации(нечетные, четные и простые)
     if filter_type == EVEN:
         return list(filter(lambda x: x % 2 == 0, numbers))
     elif filter_type == ODD:
         return list(filter(lambda x: x % 2 != 0, numbers))
     elif filter_type == PRIME:
-        def is_prime(n):
-            if n <= 1:
-                return False
-            elif n <= 3:
-                return True
-            elif n % 2 == 0 or n % 3 == 0:
-                return False
-            i = 5
-            while i * i <= n:
-                if n % i == 0 or n % (i + 2) == 0:
-                    return False
-                i += 6
-            return True
         return list(filter(is_prime, numbers))
 
 # Пример использования функций:
-#if__name__ == "__main__":
-print("Квадраты чисел:", power_numbers(1,2,3,4,5))  # Вывод: [1, 4, 25, 49]
-print("Нечетные числа:", filter_numbers([1, 2, 3], 'odd'))  # Вывод: [1, 3]
-print("Четные числа:", filter_numbers([2, 1, 3, 5, 4], "even"))  # Вывод: [2, 4]
-print("Простые числа:", filter_numbers([1,2,3,4,5,6,7,8,9],"prime"))
+if __name__ == "__main__":
+    print("Квадраты чисел:", power_numbers(1, 2, 3, 4, 5))  # Вывод: [1, 4, 9, 16, 25]
+    print("Нечетные числа:", filter_numbers([1, 2, 3], ODD))  # Вывод: [1, 3]
+    print("Четные числа:", filter_numbers([2, 1, 3, 5, 4], EVEN))  # Вывод: [2, 4]
+    print("Простые числа:", filter_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9], PRIME))  # Вывод: [2, 3, 5, 7]
